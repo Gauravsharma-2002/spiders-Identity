46b9eeb1238ca2a2e4e7c1d191eb16c61b691aa6
spiderMonk <2140113@sliet.ac.in>
2024-07-21 18:13:53 +0530

    added:section -> about-me

--- src/app/(website)/page.tsx
+++ src/app/(website)/page.tsx
@@ -1,8 +1,12 @@
 import Hero from "./components/hero"
+import AboutMe from "./components/about-me"
+import ThingsIDo from "./components/things-i-do"
 export default function Home(){
     return(
         <main>
             <Hero/>
+            <AboutMe/>
+            <ThingsIDo/>
         </main>
     )
 }
