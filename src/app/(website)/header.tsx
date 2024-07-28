d5910913d20a2f2c9db767bd32a1d4cffdece13e
spiderMonk <2140113@sliet.ac.in>
2024-07-28 17:49:13 +0530

    implemented : smooth-Scrolling and fixes minor bugs

--- src/app/(website)/header.tsx
+++ src/app/(website)/header.tsx
@@ -1,36 +1,8 @@
-"use client";
 import Link from "next/link";
 import Image from "next/image";
-import { useState } from "react";
-import { useGSAP } from "@gsap/react";
-import gsap from "gsap";
 import Magnetic from "./magnetic";
 
-export default function Header() {
-  // const isTouch = "ontouchstart" in window;
-  const [cursorPos, setCursorPos] = useState({ x: 0, y: 0 });
-  useGSAP(() => {
-    const { x, y } = cursorPos;
-    console.log(cursorPos);
-    if (x !== 0 && y !== 0) {
-      gsap.set("#me-nu", {
-        duration: 0.7,
-        scale: 1.3,
-        ease: "sine.inOut",
-        transition: "all 0.3s ease-in-out",
-      });
-    } else {
-      gsap.set("#me-nu", {
-        duration: 0.7,
-        scale: 1,
-        ease: "sine.inOut",
-        transition: " all 0.3s ease-in-out",
-        x: 0,
-        y: 0,
-      });
-    }
-  }, [cursorPos]);
-
+export default function header() {
   return (
     <header className="flex flex-row items-center justify-between max-w-7xl mx-auto px-10 py-9">
       <Link href={"/"}>
@@ -42,27 +14,8 @@ export default function Header() {
           className="h-12 opacity-0"
         />
       </Link>
-      {/* <p className="text-xl">ME-NU</p> */}
       <Magnetic>
-        <p
-          // className="text-xl"
-          // onMouseEnter={() => {
-          //   console.log("Me_NU");
-          // }}
-          id="me-nu"
-          className=" text-xl p-5"
-          onMouseEnter={(e) => {
-            setCursorPos((ocp) => ({
-              x: e.pageX - ocp.x,
-              y: e.pageY - ocp.y,
-            }));
-          }}
-          onMouseLeave={() => {
-            setCursorPos({ x: 0, y: 0 });
-          }}
-        >
-          ME-NU
-        </p>
+        <p className="text-xl p-5 zoom-button">ME-NU</p>
       </Magnetic>
     </header>
   );
